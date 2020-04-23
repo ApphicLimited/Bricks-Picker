1b8b5bfb6d5eb52d183599ad09806995f4130baa
Firat Anik <firat.anik.fa@gmail.com>
2020-04-23 09:46:59 +0000

    Second Commit
    second commit

--- Bricks-Picker/Assets/Scripts/UIItems/LevelBar.cs
+++ Bricks-Picker/Assets/Scripts/UIItems/LevelBar.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class LevelBar : MonoBehaviour
 {
+    public Text TextLevel;
+    public Image ImageLevelBar;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,6 @@ public class LevelBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ImageLevelBar.fillAmount = Vector3.Distance(GameManager.instance.PlayerManager.StartTransform.position, GameManager.instance.PlayerManager.EndTransform.position);
     }
 }
