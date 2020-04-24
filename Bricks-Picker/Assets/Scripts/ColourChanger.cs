2e6e6c0c6fd4eeffd8347a0992ac8e426e76f429
Ebru Çakar <ebru_cakar_11@hotmail.com>
2020-04-24 15:46:46 +0300

    UI düzenlemesi yapıldı. ClourChanger Prefabı oluşturuldu.

--- Bricks-Picker/Assets/Scripts/ColourChanger.cs
+++ Bricks-Picker/Assets/Scripts/ColourChanger.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class ColourChanger : MonoBehaviour
 {
     public BaseColour MainColour;
+    public MeshRenderer MyBase, Aura;
+    public ParticleSystem Particles;
+
+    private void Start()
+    {
+        MyBase.material.color = GameManager.instance.ColourController.BaseColours[(int)MainColour].Colour;
+        Aura.material.color = GameManager.instance.ColourController.BaseColours[(int)MainColour].Colour;
+        Particles.startColor = GameManager.instance.ColourController.BaseColours[(int)MainColour].Colour;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
